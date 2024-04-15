795ff1e41463aa95d059865e7146860020d3b787
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-04-15 10:34:40 +0200

    [Terminé] Job05

---
+++ jour01/job05/script.js
@@ -0,0 +1,9 @@
+function afficherJoursSemaines() {
+    const joursSemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+    
+    for (const jour of joursSemaines){
+        console.log(jour);
+    }
+
+}
+afficherJoursSemaines()
