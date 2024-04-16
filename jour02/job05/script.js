49a1f5d2814eef3dd766d75a0951d756ab4a224f
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-04-16 16:11:37 +0200

    [Terminé] Job05

--- jour02/job05/script.js
+++ jour02/job05/script.js
@@ -1,9 +1,17 @@
-function afficherJoursSemaines() {
-    const joursSemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
-    
-    for (const jour of joursSemaines){
-        console.log(jour);
-    }
+window.addEventListener('scroll', function () {
+    const scrollHeight = document.documentElement.scrollHeight;
+    const clientHeight = document.documentElement.clientHeight;
+    const scrollTop = window.scrollY;
+    const scrollPercentage = (scrollTop / (scrollHeight - clientHeight)) * 100;
 
-}
-afficherJoursSemaines()
+    const footer = document.getElementById('footer');
+    if (scrollPercentage <= 25) {
+        footer.style.backgroundColor = '#ff5a00';
+    } else if (scrollPercentage <= 50) {
+        footer.style.backgroundColor = '#ff9a00';
+    } else if (scrollPercentage <= 75) {
+        footer.style.backgroundColor = '#ffce00';
+    } else {
+        footer.style.backgroundColor = '#f0ff00';
+    }
+});
